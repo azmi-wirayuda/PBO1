e81c23bcea731a227f2d49c268edf9fe7f78e146
azmi-wirayuda <azmiwirayuda602@gmail.com>
2025-06-10 08:39:05 +0800

    menambahkan objek

---
+++ src/pbo1/latihan12313.java
@@ -0,0 +1,16 @@
+
+package pbo1;
+
+public interface latihan12313 {
+    public class (string[] argas){
+        kalkulator = new kalkulator();
+        
+        double num1 = 12.0;
+        double num2 = 0;
+        
+            System.out.println("hasil: " + kalkulator.tambah, num1, num2);
+            System.out.println("hasil: " + kalkulator.tambah, num1, num2);
+            System.out.println("hasil: " + kalkulator.tambah, num1, num2);
+            System.out.println("hasil: " + kalkulator.tambah, num1, num2);
+    }
+}
